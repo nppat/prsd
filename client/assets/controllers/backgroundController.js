7f004b87da1b30234efa130c8f52d00091d256b9
Patrick Adamson <nppat@hotmail.com>
2017-02-24 21:34:03 -0600

    redo index.html, add home3.html for bg-img use, remove bg-vid, update CSS

---
+++ client/assets/controllers/backgroundController.js
@@ -0,0 +1,7 @@
+app.controller('BackgroundController', function($scope) {
+  $scope.bodyStyle = {
+  	'background': "url('http://d1vl6bnef6sopf.cloudfront.net/ice_sunset.jpg') no-repeat center center",
+  	'background-size': 'cover',
+  	'min-height': '100%'
+};
+});
