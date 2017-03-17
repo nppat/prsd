cf8d6686139c0abb43d0a45d50a646d4a9a8ad5e
Patrick Adamson <nppat@hotmail.com>
2017-03-17 12:21:39 -0500

    mailgun update, remove platteriver email from send list, not verfied yet

--- server/controllers/form_emails.js
+++ server/controllers/form_emails.js
@@ -11,8 +11,7 @@ function NationalController() {
 
 		// Gather up email from form
 		var mailData = {
-				    		to: 'nppat@hotmail.com', // list of receivers
-							// platteriverscubadivers@gmail.com, csack99@hotmail.com,
+				    		to: 'csack99@hotmail.com, nppat@hotmail.com', // list of receivers
 							// from: req.body.email,
 				    		subject: req.body.diver1_first_name + ' ' + req.body.diver1_last_name +' and ' + req.body.diver2_first_name + ' ' + req.body.diver2_last_name + ' have registered for Nationals', // Subject line
 				    		text:
