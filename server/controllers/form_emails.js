58389c6e670bb5af95593cf7c80e7c6e54f01e51
Patrick Adamson <nppat@hotmail.com>
2017-03-20 11:19:22 -0500

    mailgun configure

--- server/controllers/form_emails.js
+++ server/controllers/form_emails.js
@@ -7,7 +7,7 @@ function NationalController() {
 
 		// create transporter
 		// send mail with password confirmation
-		var transporter = nodemailer.createTransport('smtps://postmaster@mail.platteriverscubadivers.com:e4945d760f5c7db341726ad934b668d1@smtp.mailgun.org');
+		var transporter = nodemailer.createTransport('smtps://postmaster@mail.platteriverscubadivers.com:e4945d760f5c7db341726ad934b668d1');
 
 		// Gather up email from form
 		var mailData = {
