a0f6fa700e023c70dc1aecfb71c3f429996a01f4
Patrick Adamson <nppat@hotmail.com>
2017-03-29 11:07:25 -0500

    mailgun-js setup

--- server/controllers/joins.js
+++ server/controllers/joins.js
@@ -1,41 +1,70 @@
 // Require nodemailer module to send email
-var nodemailer = require('nodemailer');
+// var nodemailer = require('nodemailer');
+
+//mailgun-js
+var api_key = 'key-b7b810188486857abbbe75791cec2732';
+var domain = 'mail.platteriverscubadivers.com';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 function JoinController() {
 
 	this.send_email = function(req,res) {
 
-		// create transporter
-		// send mail with password confirmation
+		// gather what's being sent out from form
+		var data = {
+			from: 'postmaster@mail.platteriverscubadivers.com',
+    		to: 'nppat@hotmail.com', // list of receivers platteriverscubadivers@gmail.com,
+    		subject: req.body.name + ' wants to join PRSD', // Subject line
+    		text: req.body.comments // plaintext body
+		};
 
-		var transporter = nodemailer.createTransport({
-			service: 'Gmail',
-			auth: {
-				user: 'platteriverscubadivers@gmail.com',
-				pass: 'scubadive17'
-			}
-		});
+		console.log(data); // log what's being sent out
 
-		// Gather up email from form
-		var mailData = {
-							from: 'platteriverscubadivers@gmail.com',
-				    		to: 'platteriverscubadivers@gmail.com, nppat@hotmail.com', // list of receivers
-				    		subject: req.body.name + ' wants to join PRSD', // Subject line
-				    		text: req.body.comments // plaintext body
-				    	};
-
-		console.log(mailData);
-
-		// Send email
-		transporter.sendMail(mailData, function(error, response) {
+		// send mail
+		mailgun.messages().send(data, function(error, body) {
 			if(error){
+				console.log(body);
 				console.log('[SERVER CONTROLLER JOINS ---> Send Email - ERROR] -- Email not sent');
 				res.json({ error: 'Email not sent'});
 			} else {
+				console.log(body);
 				console.log('[SERVER CONTROLLER JOINS ---> Send Email - SUCCESS] -- Email successully sent');
 				res.json({ success: 'Email has been sent.' })
 			}
 		});
+
+		// create transporter
+		// send mail with password confirmation
+
+	// 	var transporter = nodemailer.createTransport({
+	// 		service: 'Gmail',
+	// 		auth: {
+	// 			user: 'platteriverscubadivers@gmail.com',
+	// 			pass: 'scubadive17'
+	// 		}
+	// 	});
+	//
+	// 	// Gather up email from form
+	// 	var mailData = {
+	// 						from: 'platteriverscubadivers@gmail.com',
+	// 			    		to: 'platteriverscubadivers@gmail.com, nppat@hotmail.com', // list of receivers
+	// 			    		subject: req.body.name + ' wants to join PRSD', // Subject line
+	// 			    		text: req.body.comments // plaintext body
+	// 			    	};
+	//
+	// 	console.log(mailData);
+	//
+	// 	// Send email
+	// 	transporter.sendMail(mailData, function(error, response) {
+	// 		if(error){
+	// 			console.log('[SERVER CONTROLLER JOINS ---> Send Email - ERROR] -- Email not sent');
+	// 			res.json({ error: 'Email not sent'});
+	// 		} else {
+	// 			console.log('[SERVER CONTROLLER JOINS ---> Send Email - SUCCESS] -- Email successully sent');
+	// 			res.json({ success: 'Email has been sent.' })
+	// 		}
+	// 	});
+
 	};
 }
 
