0509a078caa6643f1926bc7916a2e60a0eaea937
Patrick Adamson <nppat@hotmail.com>
2017-04-01 15:10:32 -0500

    emailjs setup

--- server/controllers/joins.js
+++ server/controllers/joins.js
@@ -1,4 +1,4 @@
-// Require nodemailer module to send email
+// Require emailjs to send email
 var email = require('emailjs');
 
 function JoinController() {
@@ -8,33 +8,14 @@ function JoinController() {
 		// create transporter
 		// send mail with password confirmation
 
-// 		var transporter = nodemailer.createTransport({
-// 			service: 'Mailgun',
-// 			auth: {
-// 				user: 'postmaster@mail.platteriverscubadivers.com',
-// 				pass: 'e4945d760f5c7db341726ad934b668d1'
-// 			}
-// 		});
-
-		// var transporter = nodemailer.createTransport("SMTP", {
-		//   service: "Gmail",
-		//   auth: {
-		// 	XOAuth2: {
-		// 	  user: "platteriverscubadivers@gmail.com", // Your gmail address.
-		// 	  clientId: "430747394417-3uun5eipbsokv6r3bl2iuuatl2urv1mg.apps.googleusercontent.com",
-		// 	  clientSecret: "8x1ORkbdk6_ETHT8_o9oAu1G",
-		// 	  refreshToken: "1/gu-V1YTlHmDxQkHqQiugT2xF_Skvr33LBVc_3tk6D8w"
-		// 	}
-		//   }
-		// });
-
 		var server 	= email.server.connect({
 		   user:    "postmaster@mail.platteriverscubadivers.com",
 		   password:"e4945d760f5c7db341726ad934b668d1",
 		   host:    "smtp.mailgun.org",
 		   ssl:     true
 		});
-
+		
+		// Gather up email from form
 		// send the message and get a callback with an error or details of the message that was sent
 		server.send({
 		   text:    req.body.comments,
