21953cf36fb14642bd211dece9a7de49564a5b2a
Patrick Adamson <nppat@hotmail.com>
2017-04-01 16:02:32 -0500

    emailjs setup

--- server/controllers/joins.js
+++ server/controllers/joins.js
@@ -13,7 +13,7 @@ function JoinController() {
 		   password:"e4945d760f5c7db341726ad934b668d1",
 		   host:    "smtp.mailgun.org",
 		//    ssl:     true
-		   tls: false
+		   tls: {ciphers: "SSLv3"}
 		});
 
 		// Gather up email from form
